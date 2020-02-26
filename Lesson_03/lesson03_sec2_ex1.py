7f94fed1075d36a9f76c307d6e416d3456bdc109
Dehvon Curtis <dcurtis@adobe.com>
2020-02-26 16:22:47 -0700

    lesson03_sec1_ex1.py complete

--- Lesson_03/lesson03_sec2_ex1.py
+++ Lesson_03/lesson03_sec2_ex1.py
@@ -2,6 +2,9 @@ favorite_snack = "butterfinger"
 
 def snack_check(snack):
     if snack == favorite_snack:
-        print(True)
+        print(f'{snack} is your favorite snack!')
     elif snack != favorite_snack:
-        print(False)
+        print(f'{snack} is NOT your favorite snack!')
+
+which = input("Snack? ")
+snack_check(which)
