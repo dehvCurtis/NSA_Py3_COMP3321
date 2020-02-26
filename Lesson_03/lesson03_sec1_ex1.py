9c56a284b5b778b8de2840874f4488692fff425e
Dehvon Curtis <dcurtis@adobe.com>
2020-02-26 16:19:48 -0700

    commit

--- Lesson_03/lesson03_sec1_ex1.py
+++ Lesson_03/lesson03_sec1_ex1.py
@@ -4,10 +4,8 @@ num_list = [9.42,5.67,3.25,13.41,17.50]
 def you_won():
     choice = random.choice(num_list)
     if choice < 10:
-        print('Choice less than 10')
-        print(f'Choice: {choice}')
+        print(True)
     elif choice > 10:
-        print('Choice greater than 10')
-        print(f'Choice: {choice}')
+        print(False)
 
 you_won()
---
+++ Lesson_03/lesson03_sec1_ex2.py
@@ -0,0 +1,7 @@
+favorite_snack = "butterfinger"
+
+def snack_check(snack):
+    if snack == favorite_snack:
+        print(True)
+    elif snack != favorite_snack:
+        print(False)
