67273bdff3f7605058c11dec70c83e69ddc92ecb
Dehvon Curtis <dcurtis@adobe.com>
2020-02-26 11:04:39 -0700

    commit

--- lesson02_sec3_ex3.py
+++ lesson02_sec3_ex3.py
@@ -1,6 +1,11 @@
-def all_the_snacks(snack, spacer):
-    print((snack + spacer) * 100)
 
+
+def all_the_snacks(snack, spacer):
+    print((snack + spacer) * num)
+
+num = input('Declare number of times to output >')
+num = int(num)
 spacer = input('Insert word spacer >')
 my_snack = input('Pick a snack >')
+
 all_the_snacks(my_snack, spacer)
