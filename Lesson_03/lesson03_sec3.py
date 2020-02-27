1a7ef1903bbd6047070702e7e3deee3b9ca490ae
Dehvon Curtis <dcurtis@adobe.com>
2020-02-27 11:38:45 -0700

    commit

---
+++ Lesson_03/lesson03_sec3.py
@@ -0,0 +1,21 @@
+
+# ex 1
+# snack = 'cookie'
+# count = 0
+# while count < 10:
+#     print(snack)
+#     count += 1
+
+# ex 2
+# snack = 'cookie'
+# count = 0
+# while count < 10:
+#     count += 1
+#     print(snack * count)
+
+# ex 3
+# snack = 'cookie'
+# count = 0
+# while count < 100:
+#     count += 1
+#     print(snack * 100)
