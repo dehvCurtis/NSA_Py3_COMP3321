03c64421b71390c3a34abea70c3eed26cc78885a
Dehvon Curtis <dcurtis@adobe.com>
2020-02-27 11:59:02 -0700

    commit

---
+++ Lesson_03/lesson03_sec4.py
@@ -0,0 +1,35 @@
+# ex 1
+# string = "blood-oxygenation level dependent functional magnetic resonance imaging"
+# count = 0
+# for letter in string:
+#     count += 1
+#     print(letter)
+#
+# print(count, ' chars')
+
+# ex 2
+# glist = ['milk','bread','detergent']
+# for item in glist:
+#     print(f'Note to self, buy: {item}')
+
+# ex 3
+# glist = ['milk','bread','detergent']
+# numb = 0
+# for item in glist:
+#     print(numb + 1, item)
+#     numb += 1
+
+# ex 4
+# glist = ['milk','bread','detergent','donut','juice','cookie']
+# snack = input('Fav snack? ')
+# for item in glist:
+#     print(item)
+#     if item == snack:
+#         print(f'Found your snack: {snack}')
+#         break
+
+# ex 5
+string = "blood-oxygenation level dependent functional magnetic resonance imaging"
+for word in string:
+    print(string.split())
+    break
