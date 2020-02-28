84aa9debcc5c3f5d8931c4d2d44b16be02ea516a
Dehvon Curtis <dcurtis@adobe.com>
2020-02-28 15:54:02 -0700

    Lesson 4 complete

---
+++ Lesson_04/lesson04_sec1.py
@@ -0,0 +1,10 @@
+# ex 2
+list = [1,2,3,4,4,4,4,2,3,4,6,7,8,9]
+animals = ['cow','turtle','cow','horse','duck']
+print(f'Printing list: {animals}')
+def duplicates(arg):
+    list1 = [i for i in animals]
+    print(set(list1))
+duplicates(animals)
+
+# ex 3
