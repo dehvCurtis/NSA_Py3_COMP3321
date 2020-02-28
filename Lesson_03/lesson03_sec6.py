29a61a62985d43791daae92917d43c46ba4b40da
Dehvon Curtis <dcurtis@adobe.com>
2020-02-28 13:36:56 -0700

    lesson 3 complete

---
+++ Lesson_03/lesson03_sec6.py
@@ -0,0 +1,31 @@
+# u 2
+# word = 'skyscraper'
+# letter = 'c'
+# def getIndex():
+#     global letter
+#     for ind in word:
+#         if ind == 'c':
+#             print(letter)
+#         else:
+#             pass
+# getIndex()
+
+# u 3
+# def shouter():
+#     sentence = input('Insert Sentence: ')
+#     sentence = sentence.split()
+#     for string in sentence:
+#         print(string.upper())
+# shouter()
+
+# u 4
+# sentence = "this is a sentence i wrote in order to write some codes"
+# sentence = sentence.split()
+# length = len(sentence)
+#
+# def extract_longer(length, sentence):
+#     for word in sentence:
+#         if len(word) > 4:
+#             print(f'More than 3 chars: {word}')
+#
+# extract_longer(length, sentence)
