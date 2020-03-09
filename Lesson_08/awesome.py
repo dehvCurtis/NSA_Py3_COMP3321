68d0f981fe7c97c0da4455b0f729576b9b86fd7f
Dehvon Curtis <dcurtis@adobe.com>
2020-03-09 16:01:27 -0600

    commit

---
+++ Lesson_08/awesome.py
@@ -0,0 +1,19 @@
+
+contents = '''
+class Awesome(object):
+    def __init__(self, awesome_thing):
+        self.thing = awesome_thing
+    def __str__(self):
+        return f'{self.thing}'
+
+def cool(group):
+    return f'Everything cool when you a part of {group}'
+
+if __name__ == '__main__':
+     a = Awesome("EVERYTHING")
+     print(a)
+'''
+
+with open('test.py','w') as f:
+    f.write(contents)
+
